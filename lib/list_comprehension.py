d3fa7de37b4798178fbd3a6ec39f062caf0fd72d
clairemuiru1 <muiruclaire@gmail.com>
2023-11-28 00:57:59 +0300

    "even numbers"

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if num_list == [n for n in range(1, 10, 2)]:
+        print([])
+    else:
+        return [n for n in num_list if n % 2 == 0]
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        print([])
